0e4b4904df2b6d83c2fe238269a15d9431c99e96
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2022-11-05 00:12:01 -0500

    category item component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import CategoryItem from './components/category-item/category-item.component';
 import './categories.styles.scss';
 
 const App = () => {
 
   //So instead of hardcoding out these 4 other div sections, we're going to initialize some variable array. And this is going to be an array that contains objects that reflect what it is that we have in here.
+  //array of json objects
   const categories = [
     {
       "id": 1,
@@ -11,12 +13,12 @@ const App = () => {
     },
     {
       "id": 2,
-      "title": "coats & jackets",
+      "title": "jackets",
       "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
     },
     {
       "id": 3,
-      "title": "shoes",
+      "title": "sneakers",
       "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
     },
     {
@@ -29,20 +31,14 @@ const App = () => {
       "title": "mens",
       "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
     }
-
-  ];
+];
 
   return (
     <div className="categories-container">
       {/* So we know that inside of our categories container, we're going to map over the categories. So remember, wrap it in your squiggly brackets, then say categories map and you're going to pass at a function where you get the category.  */}
-        {categories.map(({title}) => (
-          <div className="category-container">
-          <div className='background-image' />
-            <div className="category-body-container">
-              <h2>{title}</h2>
-              <p>Shop Now</p>
-            </div>
-          </div>
+      {/*destructure off title, id, imageUrl*/}  
+        {categories.map((category) => (
+          <CategoryItem key={category.id} category={category}  />
         ))}
 
     {/* We hardcoded this at first because it was the easier thing to do, but as we scale, this definitely doesn't make sense. Essentially we're repeating the same code here where these values, where it's pretty much just the <h2>...<h2>s are the only places where it's essentially different. Maybe the image is different, but we haven't even tackled that yet. But we can already tell that this value is going to be repeated just using a different value. So this is a good place for us to actually start optimizing a little bit. So instead of hard coding out these for other sections, what we can do is we can actually initialize some variable array and I want to call this thing the directories or the categories. And this is going to be an array that contains objects that reflect what it is that we have in here.  */}
---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,24 @@
+import './category-item.styles.scss'
+
+const CategoryItem = ({ category }) => {
+
+    const {imageUrl, title} = category;
+
+    return (
+        <div className="category-container">
+        {/* What I want you to know is that when you are trying to append, let's say, some additional CSS that's dynamic, you can actually do that in React by passing it as a 'style' object. So on any different component in your JSX, you can actually append custom styles by giving it this 'style' field. This style, if you'll takes an object where the key is the CSS property that you want to modify. So in our particular case, I want backgroundImage. So with background image, the value that we need to pass it is going to be a string of URL and in the brackets is the actual URL string value. So here I'm going to use object structuring. So I want to use the back ticks. And with the object structuring I'm going to pass in image URL. If you're unfamiliar with de structuring, all this is really doing is it's just allowing us to use a string variable inside of another string. And this essentially what will happen is that we will apply a custom style onto this specific HTML element.*/}  
+            <div 
+                className='background-image' 
+                style={{
+                    backgroundImage: `url(${imageUrl})`
+                }} 
+            />
+            <div className="category-body-container">
+                <h2>{title}</h2>
+                <p>Shop Now</p>
+            </div>
+        </div>
+    )
+}
+
+export default CategoryItem
