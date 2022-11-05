bd65b79d48fc6ae09a2fff936db772ff10da922b
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2022-11-05 14:53:54 -0500

    Nested routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import Home from './routes/home/home.component'
+import { Routes, Route } from 'react-router-dom';
+import Home from './routes/home/home.component';
 
-const App = () => {
-  return(
-    <Home />
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1></h1>
+      </div>
+    </div>
   )
 }
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
+  );
+};
 
 export default App;
