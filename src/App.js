ad6e62cf26eab420d496a8be8ff1220c12959bd0
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2022-11-11 14:22:37 -0600

    setting up new shop page

--- src/App.js
+++ src/App.js
@@ -6,11 +6,14 @@ import Home from './routes/home/home.component';
 
 import Authentication from './routes/authentication/authentication.component';
 
+import Shop from './routes/shop/shop.component';
+
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
+        <Route path='shop' element={<Shop/>} />
         <Route path='auth' element={<Authentication/>} />
       </Route>
     </Routes>
