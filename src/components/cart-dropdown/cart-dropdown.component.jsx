dcad555a9a5ee81827e454c94e7223d74409b449
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2022-11-11 15:52:51 -0600

    implementing add to cart feature

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,16 +1,19 @@
 import './cart-dropdown.styles.scss';
 
+import CartItem from '../cart-item/cart-item.component';
+
 import Button from '../button/button.component';
 
 const CartDropdown = () => {
 
     return (
         <div className='cart-dropdown-container'>
-            <div className='cart-items'/>
-            <Button> GO TO CHECKOUT </Button>
+            <div className='cart-items'>
+                
+            </div>
+            <Button> CHECKOUT </Button>
         </div>
     );
-
 };
 
 export default CartDropdown;
