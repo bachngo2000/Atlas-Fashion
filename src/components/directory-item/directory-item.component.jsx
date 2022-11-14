be624b80d4514daf92b0a00e9a34ad06833b3276
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2022-11-13 22:42:49 -0600

    worked on clickable item image

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,11 +1,16 @@
-import './directory-item.styles.scss'
+import './directory-item.styles.scss';
+
+import { useNavigate } from 'react-router-dom';
 
 const DirectoryItem = ({ category }) => {
 
-    const {imageUrl, title} = category;
+    const {imageUrl, title, route} = category;
+    const navigate = useNavigate();
+
+    const onNavigateHandler = () => navigate(route);
 
     return (
-        <div className="directory-item-container">
+        <div className="directory-item-container" onClick={onNavigateHandler}>
         {/* What I want you to know is that when you are trying to append, let's say, some additional CSS that's dynamic, you can actually do that in React by passing it as a 'style' object. So on any different component in your JSX, you can actually append custom styles by giving it this 'style' field. This style, if you'll takes an object where the key is the CSS property that you want to modify. So in our particular case, I want backgroundImage. So with background image, the value that we need to pass it is going to be a string of URL and in the brackets is the actual URL string value. So here I'm going to use object structuring. So I want to use the back ticks. And with the object structuring I'm going to pass in image URL. If you're unfamiliar with de structuring, all this is really doing is it's just allowing us to use a string variable inside of another string. And this essentially what will happen is that we will apply a custom style onto this specific HTML element.*/}  
             <div 
                 className='background-image' 
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,8 +1,45 @@
-import DirectoryItem from '../directory-item/directory-item.component'
+import DirectoryItem from '../directory-item/directory-item.component';
 
-import './directory.styles.scss'
+import './directory.styles.scss';
 
-const Directory = ({categories}) => {
+const categories = [
+  {
+    "id": 1,
+    "title": "Hats",
+    "imageUrl": "https://manofmany.com/wp-content/uploads/2017/10/Guide-to-Wearing-Mens-Hats-With-Suits-20.jpg",
+    "route": 'shop/hats'
+  },
+  {
+    "id": 2,
+    "title": "Coats & Jackets",
+    "imageUrl": "https://suitaddict.com/wp-content/uploads/2018/01/E1E82A31-6B9A-4062-9FDC-21D692AA1A04.jpeg",
+    "route": 'shop/jackets'
+  },
+  {
+    "id": 3,
+    "title": "Shoes",
+    "imageUrl": "https://m.media-amazon.com/images/G/01/2022/mens-shoes/MENS-FALL-BOOTS-NEW-ARRIVALS-OCTOBER-HERO-1440x700.jpg",
+    "route": 'shop/sneakers'
+
+  },
+  {
+    "id": 4,
+    "title": "Womens",
+    "imageUrl": "https://thestateofwomen.com/wp-content/uploads/2016/03/women-shopping.jpg",
+    "route": 'shop/womens'
+
+  },
+  {
+    "id": 5,
+    "title": "Mens",
+    "imageUrl": "https://www.dmarge.com/wp-content/uploads/2020/12/mens-clothing-sale-feature.jpg",
+    "route": 'shop/mens'
+
+  },
+];
+
+const Directory = () => {
+  
     return (
         <div className="directory-container">
         {/* So we know that inside of our categories container, we're going to map over the categories. So remember, wrap it in your squiggly brackets, then say categories map and you're going to pass at a function where you get the category.  */}
